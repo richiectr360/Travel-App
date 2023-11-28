6fb7f873bc5f0ab36a5725c386881505f5c0731d
richiectr360 <richie.amornchotsingh@gmail.com>
2023-11-27 21:51:24 -0600

    Feat: Added Navbar, category list, and hotel listing page

--- travel-app/src/components/HotelCard/HotelCard.jsx
+++ travel-app/src/components/HotelCard/HotelCard.jsx
@@ -1,22 +1,25 @@
 import "./HotelCard.css";
 
-export const HotelCard = () => {
+export const HotelCard = ({hotel}) => {
+
+    const { _id, name, image, address, state, rating, price} = hotel;
+
     return (
         <div className="relative hotelcard-container shadow cursor-pointer">
             <div>
-                <img className="img" src="http://a0.mustache.com/im/pictures/miso/Hosting-26117817/original/9da40e3c-5846-4359-bb41-05c27b09a8f5.jpeg?im_w=720" alt="hotelcard"/>
+                <img className="img" src={image} alt={name}/>
                 <div className="hotelcard-details">
                     <div className="d-flex align-center">
-                        <span className="location">Bir, Himachal Pradesh</span>
+                        <span className="location">{address}, {state}</span>
                         <span className="rating d-flex align-center">
                             <span class="material-symbols-outlined">star</span>
-                            <span> 4.3</span>
+                            <span> {rating}</span>
                         </span>
                     </div>
 
-                    <p className="hotel-name">Sukoon Bag</p>
+                    <p className="hotel-name">{name}</p>
                     <p className="price-details">
-                        <span className="price">Rs. 3500</span>
+                        <span className="price">Rs. {price} </span>
                         <span>night</span>
                     </p>
                 </div>
